f2efe9c90a7b4ad80999a53af5d3ffc2167bee55
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-06-04 11:13:21 +0530

    chore: remove empty bundle files - app has no frontend assets to build

--- hooks.py
+++ hooks.py
@@ -18,10 +18,6 @@ after_install = "projects.install.after_install"
 # DocTypes
 # ------------------------------
 
-# Bundled JS/CSS that esbuild scans
-app_include_js = "projects.bundle.js"
-app_include_css = "projects.bundle.css"
-
 # Website
 # ------------------------------
 website_route_rules = [
--- projects/public/js/projects.bundle.js
+++
@@ -1,2 +0,0 @@
-// Projects app bundle
-// Frappe esbuild requires at least one bundle file to build assets
